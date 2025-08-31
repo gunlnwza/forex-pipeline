65695c95cabcc25fda95b266aaddcda517645747
Natachat Teecharcharn <nteechar@Natachats-MacBook-Air.local>
2025-08-31 22:52:41 +0700

    Feat: add ChatGPT ideas to src

--- src/backtester.py
+++ src/backtester.py
@@ -1,1 +1,17 @@
 # Runs strategies on historical data
+
+class Backtester:
+    def __init__(self):
+        pass
+
+    def run(self):
+        "return plot-able results"
+        pass
+
+class Results:
+    def __init__(self):
+        pass
+
+    def plot():
+        pass
+    
--- src/data_loader.py
+++ src/data_loader.py
@@ -1,1 +1,9 @@
 # Download & cache from Polygon/OANDA
+
+class DataLoader:
+    def __init__(self):
+        pass
+
+    def download(self, tickers: list[str]):
+        "look at Polygon, Oanda, or AlphaVantage"
+        pass
